7382cc3fa032891b1cf36b3ffee863055818eca7
26fati <fatibeautiful1@gmail.com>
2023-07-15 09:12:20 -0700

    fix some errors

--- shell.c
+++ shell.c
@@ -6,22 +6,24 @@ int main()
   char * prompt = " ($) ";
   char * command;
   size_t command_size = 1024;
-  int status;
+  int status, command_len;
   char* argv[2]; 
+  pid_t pid;
 
 while(1) {
   write(0, prompt , 6);
 
-  int command_len = getline(&command, &command_size, stdin);
+  command_len = getline(&command, &command_size, stdin);
   if (command_len == -1) {
       exit(EXIT_FAILURE);
     }
   command[command_len - 1] = '\0'; 
-  // write(0, command, command_size);
+  /* write(0, command, command_size);
+   */
 
   argv[0] = command;
   argv[1] = NULL;
-  pid_t pid = fork();
+  pid = fork();
   if (pid == 0) {
     int res = execve(command,argv, NULL);
     if (res == -1) perror("./hsh");
