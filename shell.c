edf56988cd90c1ca0a663e36354fc75cc737536d
louzanizineddine <louzanizineddine@gmail.com>
2023-07-15 12:12:04 +0200

    more improvments on task 1

--- shell.c
+++ shell.c
@@ -1,37 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
-
 
 int main()
 
 {
-  char * prompt = "($) ";
+  char * prompt = " ($) ";
   char * command;
   size_t command_size = 1024;
   int status;
   char* argv[2]; 
-  char* envp[1];
-  
 
 while(1) {
-  write(0, prompt , 5);
+  write(0, prompt , 6);
 
   int command_len = getline(&command, &command_size, stdin);
   if (command_len == -1) {
       exit(EXIT_FAILURE);
     }
   command[command_len - 1] = '\0'; 
-  printf("\n");
-  write(0, command, command_size);
-  printf("\n");
-
+  // write(0, command, command_size);
 
   argv[0] = command;
   argv[1] = NULL;
-  envp[0] = NULL;
   pid_t pid = fork();
   if (pid == 0) {
-    int res = execve(_strjoin("/bin/", command), argv, NULL);
+    int res = execve(command,argv, NULL);
     if (res == -1) perror("./hsh");
   }
   else {
@@ -39,4 +31,6 @@ while(1) {
   }
 
   }
+
+  return (0);
 }
